3c6d89057004965f59b19262d7eeb784fbec702b
trecain <developedbycain@yahoo.com>
2018-11-05 16:26:40 -0800

    got the chef route working

--- PraiseProvisionsAPI/PraiseProvisionsAPI/Controllers/ChefController.cs
+++ PraiseProvisionsAPI/PraiseProvisionsAPI/Controllers/ChefController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PraiseProvisionsAPI.Data;
+using PraiseProvisionsAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,11 +13,17 @@ namespace PraiseProvisionsAPI.Controllers
     [Route("api/[controller]")]
     public class ChefController : ControllerBase
     {
+        public PraiseDBContext _context { get; set; }
+        public ChefController(PraiseDBContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/<controller>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<Chef>>Get()
         {
-            return new string[] { "value1", "value2" };
+            return _context.Chefs;
         }
 
         // GET api/<controller>/5
---
+++ PraiseProvisionsAPI/PraiseProvisionsAPI/Migrations/20181106002127_Initial.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PraiseProvisionsAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chefs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chefs", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    ChefID = table.Column<int>(nullable: false),
+                    RestaurantID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => new { x.ChefID, x.RestaurantID });
+                    table.ForeignKey(
+                        name: "FK_Favorites_Chefs_ChefID",
+                        column: x => x.ChefID,
+                        principalTable: "Chefs",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Favorites_Restaurants_RestaurantID",
+                        column: x => x.RestaurantID,
+                        principalTable: "Restaurants",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Chefs",
+                columns: new[] { "ID", "City", "FirstName", "LastName" },
+                values: new object[] { 1, "Albany", "Paula", "Deen" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorites_RestaurantID",
+                table: "Favorites",
+                column: "RestaurantID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorites");
+
+            migrationBuilder.DropTable(
+                name: "Chefs");
+
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+        }
+    }
+}
