e080ac92a70837bc1acad33aa53c21ccc36f89e5
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-11-06 16:35:22 -0800

    Web app is now linked to the custom API ("PraiseAPI")

---
+++ PraiseProvisions/PraiseProvisions/Controllers/ChefsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PraiseProvisions.Controllers
+{
+    [Route("[controller]")]
+    public class ChefsController : Controller
+    {
+        [HttpGet("chef")]
+        public async Task<IActionResult> Chef()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("https://praiseapi.azurewebsites.net");
+                    var response = await client.GetAsync($"/api/chef");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+
+                    return Ok(stringResult);
+                }
+                catch (HttpRequestException e)
+                {
+                    return BadRequest($"Sorry, getting chefs from PraiseAPI: {e.Message}");
+                }
+            }
+        }
+    }
+}
