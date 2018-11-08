5fe92ed372c0dc25e0f149f6d18bcefda015255e
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-11-07 19:26:18 -0800

    created resultsController, yelpResults model, chefResults model, and a results view.

---
+++ PraiseProvisions/PraiseProvisions/Models/ChefResults.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PraiseProvisions.Models
+{
+    public class ChefResults
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("chefFirstName")]
+        public string firstName { get; set; }
+
+        [JsonProperty("chefLastName")]
+        public string lastName { get; set; }
+
+        [JsonProperty("restaurantName")]
+        public string restaurantName { get; set; }
+
+        [JsonProperty("address")]
+        public string address { get; set; }
+
+        [JsonProperty("description")]
+        public string description { get; set; }
+
+        [JsonProperty("city")]
+        public string city { get; set; }
+
+        [JsonProperty("review")]
+        public string review { get; set; }
+    }
+}
