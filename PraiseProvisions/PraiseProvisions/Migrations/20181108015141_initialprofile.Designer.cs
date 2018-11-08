15b02dadd4a1733f0e0586e2577df14daf12fcfd
thejamilah <jamilahmcw@gmail.com>
2018-11-07 18:29:34 -0800

    Create User Profile works

--- PraiseProvisions/PraiseProvisions/Migrations/20181108015141_initialprofile.Designer.cs
+++ PraiseProvisions/PraiseProvisions/Migrations/20181108015141_initialprofile.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PraiseProvisions.Data;
 
 namespace PraiseProvisions.Migrations
 {
     [DbContext(typeof(PraiseProvisionDbContext))]
-    partial class PraiseProvisionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181108015141_initialprofile")]
+    partial class initialprofile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
