02deaabae7c42ce5c769422f7f794dca7f51869f
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-11-08 17:05:44 -0800

    updated the AddFavorites controller; favorites can now be added to the DB from the results page

--- PraiseProvisions/PraiseProvisions/Controllers/AddFavoriteController.cs
+++ PraiseProvisions/PraiseProvisions/Controllers/AddFavoriteController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PraiseProvisions.Data;
 using PraiseProvisions.Models;
+using PraiseProvisions.Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +11,45 @@ using System.Threading.Tasks;
 
 namespace PraiseProvisions.Controllers
 {
-    [Route("[controller]")]
+    //[Route("[controller]")]
     public class AddFavoriteController : Controller 
     {
-        [HttpPost]
-        public IActionResult Index(int id, ChefResults chefResults)
+        private readonly PraiseProvisionDbContext _context;
+        public AddFavoriteController(PraiseProvisionDbContext context)
         {
-            return Ok();
+
+            _context = context;
+        }
+        //[HttpGet]
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Index(int id, string firstName, string lastName, string restaurantName, string address, string description, string city, string review)
+        {
+            Restaurant restaurantAdded = new Restaurant
+            {
+                Name = restaurantName,
+                Address = address,
+                Description = description
+            };
+
+            await _context.Restaurants.AddAsync(restaurantAdded);
+            await _context.SaveChangesAsync();
+
+            Restaurant restaurant = await _context.Restaurants.FirstOrDefaultAsync(x => x.Name == restaurantAdded.Name);
+
+            await _context.UserFavorites.AddAsync(new UserFavorite
+            {
+                UserProfileID = id,
+                RestaurantID = restaurant.ID
+            });
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "UserFavorites", new { id });
         }
     }
 }
---
+++ PraiseProvisions/PraiseProvisions/Models/ViewModel/RestaurantFavoritesVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PraiseProvisions.Models.ViewModel
+{
+    public class RestaurantFavoritesVM
+    {
+        public int UserID { get; set; }
+        
+        public string firstName { get; set; }
+        
+        public string lastName { get; set; }
+        
+        public string restaurantName { get; set; }
+        
+        public string address { get; set; }
+        
+        public string description { get; set; }
+        
+        public string city { get; set; }
+        
+        public string review { get; set; }
+    }
+}
