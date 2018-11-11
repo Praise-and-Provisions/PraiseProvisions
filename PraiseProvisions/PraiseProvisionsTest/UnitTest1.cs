83ac99b6e87faaaaf643def5681be3cfc1f7981e
thejamilah <jamilahmcw@gmail.com>
2018-11-11 15:08:20 -0800

    First test passed

--- PraiseProvisions/PraiseProvisionsTest/UnitTest1.cs
+++ PraiseProvisions/PraiseProvisionsTest/UnitTest1.cs
@@ -1,15 +1,21 @@
 using System;
 using Xunit;
 using PraiseProvisions;
+using PraiseProvisions.Models;
+using PraiseProvisions.Data;
+using PraiseProvisions.Controllers;
 
 namespace PraiseProvisionsTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanGetChefReviewsViaChefResultsModel()
         {
-
+            ChefResults reviews = new ChefResults
+            {
+                review = "This is a raving review"
+            };
         }
     }
 }
